5728c589e425d1133c7753597ccba53fa6381ecb
Jacob Okamoto <github@oko.io>
2013-03-27 19:48:51 -0500

    API server ready for test deployment

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='geoauth-server',
+    version='',
+    packages=['geoauth', 'geoauth.blueprint', 'geoauth.tests'],
+    url='',
+    license='',
+    author='Jacob Okamoto',
+    author_email='okam0013@umn.edu',
+    description=''
+)
